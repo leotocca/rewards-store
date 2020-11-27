6be0c4f08045da130606cdbca4f92ebc242e3957
Leonardo Toccaceli <leotoccaceli@gmail.com>
2020-11-27 09:37:33 -0300

    Updates

---
+++ precache-manifest.516c7f6c3022b28b4fc215aba261ac43.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "112ec6372adc67f7e3528c3be9af86a7",
+    "url": "/rewards-store/index.html"
+  },
+  {
+    "revision": "c6a1e796d667d6668f56",
+    "url": "/rewards-store/static/css/main.ab88a0e4.chunk.css"
+  },
+  {
+    "revision": "593ccefc4c30cfdea83e",
+    "url": "/rewards-store/static/js/2.113030bf.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/rewards-store/static/js/2.113030bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6a1e796d667d6668f56",
+    "url": "/rewards-store/static/js/main.e1e9ca03.chunk.js"
+  },
+  {
+    "revision": "5c9e3472635ccb07e47c",
+    "url": "/rewards-store/static/js/runtime-main.376726f6.js"
+  },
+  {
+    "revision": "eed908a56fa040c80d75d382e0922842",
+    "url": "/rewards-store/static/media/bg.eed908a5.jpg"
+  },
+  {
+    "revision": "048c298651f675c4b88b41bc6d533459",
+    "url": "/rewards-store/static/media/close.048c2986.svg"
+  },
+  {
+    "revision": "6ceba9abc08cac2867d0dfbe6a96cb67",
+    "url": "/rewards-store/static/media/coin.6ceba9ab.svg"
+  },
+  {
+    "revision": "86934d19de20df1ed328276e3f582d33",
+    "url": "/rewards-store/static/media/logo.86934d19.svg"
+  },
+  {
+    "revision": "5aba2fec5b8d5c9020aec3eb46cb30e0",
+    "url": "/rewards-store/static/media/searchIcon.5aba2fec.svg"
+  }
+]);
